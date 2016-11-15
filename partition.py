dddb9dfd4313b7bbc0ea7af4eab018acd620b6b8
roadway <luoshi@163.net>
2016-11-15 15:45:38 +0800

    partition.py 增加判断没有表名.字段名称的情况

--- partition.py
+++ partition.py
@@ -3,7 +3,7 @@
 import sys
 reload(sys)
 sys.setdefaultencoding('utf-8')
-s = 'employee.员工ID,employee.员工姓名 aS username, department.部门名称 As department,department.ID'
+s = 'name,性别 as sex,userID,employee.员工姓名 aS username, department.部门名称 As department,department.ID'
 print s
 s = unicode(s,"utf-8")
 print s
@@ -17,17 +17,23 @@ s=s.split(',')
 print s
 #检索数列中的field值
 n = 0
-for i in s:    
-    if len(i.partition(' AS ')[2]) > 0 :
-        s[n] = i.partition(' AS ')[2]
-    else:      
-        s[n] = i.partition('.')[2]        
+for i in s:        
+    if i.find('.') > 0: #检查是否包括表名.字段名之间那个.
+        if len(i.partition(' AS ')[2]) > 0 :
+            s[n] = i.partition(' AS ')[2]
+        else:      
+            s[n] = i.partition('.')[2]
+    else:
+        if len(i.partition(' AS ')[2]) > 0 :
+            s[n] = i.partition(' AS ')[2]
+        else:      
+            s[n] = i
     n = n + 1
 print s
 #解码后的字符串的field值
 n = 0
 for i in s:
-    print i.decode(encoding='UTF-8',errors='strict')
-    s[n] = i.decode(encoding='UTF-8',errors='strict')
+    print i.encode(encoding='UTF-8',errors='strict')
+    s[n] = i.encode(encoding='UTF-8',errors='strict')
     n = n + 1
 print s
